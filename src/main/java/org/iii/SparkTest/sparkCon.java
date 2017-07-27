7205aa22a73407f0f7dff9c0d0df131302cf8093
maskman24610 <showong24610@yahoo.com.tw>
2017-07-27 09:42:37 +0800

    練習用Maven建立java跑Spark的專案並做一個Spark小練習

---
+++ src/main/java/org/iii/SparkTest/sparkCon.java
@@ -0,0 +1,24 @@
+package org.iii.SparkTest;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+
+public class sparkCon {
+//  static SparkConf conf;
+//  static JavaSparkContext sc;
+//  public sparkCon() {
+//    conf = new SparkConf().setMaster("local");
+//    sc = new JavaSparkContext(conf);
+//  }
+  
+  public static void main(String[] args) {
+	  SparkConf conf = new SparkConf().setMaster("local").setAppName("firstSpark");
+	  JavaSparkContext sc = new JavaSparkContext(conf); 
+	  JavaRDD<String> lines = sc.textFile("test.html");
+	  JavaRDD<String> filterLines = lines.filter(line -> line.contains("鈦坦"));
+	  System.out.println(lines.count());
+	  System.out.println("The first of test.html is" +lines.first());
+	  System.out.println(filterLines.first()+", 過濾後的行數："+filterLines.count());
+  }
+}
